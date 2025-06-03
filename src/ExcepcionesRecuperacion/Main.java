3e6e14ecd82426782c5a426e4883c253686d2b55
epercar444 <epercar444@g.educaand.es>
2025-06-03 09:46:57 +0200

    RecuperaciónExcepciones

---
+++ src/ExcepcionesRecuperacion/Main.java
@@ -0,0 +1,11 @@
+package ExcepcionesRecuperacion;
+
+public class Main {
+
+	public static void main(String[] args) {
+		BoletinEjercicio3 t = new BoletinEjercicio3();
+		t.NumeroPorTeclado();
+
+	}
+
+}
